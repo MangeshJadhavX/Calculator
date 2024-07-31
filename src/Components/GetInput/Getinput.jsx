ec3e457dcd85c8e54135820b23b50b5be5d44ee6
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-07-31 16:10:24 +0530

    get the data from parent App.js and show its input value

--- src/Components/GetInput/Getinput.jsx
+++ src/Components/GetInput/Getinput.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import './Getinput.css'
 
-export const Getinput = () => {
+export const Getinput = ({displayValue}) => {
+
   
   return (
-    <div>
-      <input type="text" id='display' />
-     
+    <div className='input' >
+      <input type="text" id='display' value={displayValue} readOnly/>
     </div>
   )
 }
