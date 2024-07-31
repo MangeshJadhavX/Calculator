76169ab250058ec6e270ce9594652cef3f1b2721
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-07-31 16:06:17 +0530

    Crate a useState and pasing the useState calVal data to GetInput component and passing the getbtn method to button components with functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import { Button } from './Components/Buttons/Button';
 import { Getinput } from './Components/GetInput/Getinput';
 
 function App() {
+ 
+  const [calVal,setCalVal] = useState('')
+  
+  let getBtnValue = (buttonText)=>{
+    console.log(buttonText);
+    if (buttonText == 'C') {
+      setCalVal("");
+    } 
+    else if (buttonText === "=") {
+      let result = eval(calVal);
+      setCalVal(result);
+    }
+    else{
+      const setNewVal = calVal + buttonText;
+      setCalVal(setNewVal);
+    }
+  }
+   
   return (
     <div className="App">
       <div className='Container'>
-     <Getinput/>
-     <Button/>
+     <Getinput displayValue = {calVal}/>
+     <Button onClickVal ={getBtnValue}/>
       </div>
 
     </div>
