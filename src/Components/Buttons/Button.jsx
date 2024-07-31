32510b7bf8a20a1c841e8a855656384ab1b25e4c
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-07-31 16:07:38 +0530

    get data from parent and crates the buttons

--- src/Components/Buttons/Button.jsx
+++ src/Components/Buttons/Button.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./Button.css"
 
-export const Button = () => {
+export const Button = ({onClickVal}) => {
   
  const buttons = ['*','-','/','.','+','1','2','3','4','5','6','7','8','9','=','C','0'];  
 
   return (
     <>
     <div className="buttonContainer">
-    {buttons.map((buttonName)=>{ return <button className='btn'>{buttonName}</button>})}
+    {buttons.map((buttonName)=>{ return <button className='btn' onClick={(e)=>onClickVal(buttonName)}>{buttonName}</button>})}
     </div>    
     </>
   )
